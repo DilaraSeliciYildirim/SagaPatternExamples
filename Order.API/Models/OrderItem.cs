145226492afee7e25eb13a6d34b293878f58ff87
dilara selici <Dilara.Selici@adesso.com.tr>
2025-04-17 10:36:04 +0300

    order migration

---
+++ Order.API/Models/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Order.API.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int MyProperty { get; set; }
+
+        [Column(TypeName ="decimal(18,2)")] // toplam 16 basamak virgülden önce 2 basamak sonra
+        public decimal Price { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        public int Count { get; set; }
+    }
+}
--- Order.API/Program.cs
+++ Order.API/Program.cs
@@ -1,9 +1,32 @@
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Order.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+
+builder.Services.AddMassTransit(x => {
+
+    x.UsingRabbitMq((context, cfg) => {
+
+        cfg.Host(builder.Configuration.GetConnectionString("RabbitMQ"));
+    });
+   
+});
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
+});
+
+
 
 var app = builder.Build();
 
