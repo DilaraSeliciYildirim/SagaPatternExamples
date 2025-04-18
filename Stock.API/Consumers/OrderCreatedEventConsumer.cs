4f6d59869024d1f8cb37de6d80dc42f0638e633f
dilara selici <Dilara.Selici@adesso.com.tr>
2025-04-18 14:16:43 +0300

    StockReservedEvent StockNotReservedEvent OrderCreatedEventConsumer (Stock API)

---
+++ Stock.API/Consumers/OrderCreatedEventConsumer.cs
@@ -0,0 +1,76 @@
+﻿using Core;
+using Core.Events;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Stock.API.Models;
+
+namespace Stock.API.Consumers
+{
+    public class OrderCreatedEventConsumer : IConsumer<OrderCreatedEvent>
+    {
+        private readonly AppDbContext _appDbContext;
+        private ILogger<OrderCreatedEventConsumer> _logger;
+        private readonly ISendEndpointProvider _sendEndpointProvider; // tek alıcı varsa send
+        private readonly IPublishEndpoint _publishEndpoint; // birden çok alıcı varsa publish
+
+        public OrderCreatedEventConsumer(
+            AppDbContext appDbContext, 
+            ILogger<OrderCreatedEventConsumer> logger, 
+            ISendEndpointProvider sendEndpointProvider,
+            IPublishEndpoint publishEndpoint)
+        {
+            _appDbContext = appDbContext;
+            _logger = logger;
+            _sendEndpointProvider = sendEndpointProvider;
+            _publishEndpoint = publishEndpoint;
+        }
+
+        public async Task Consume(ConsumeContext<OrderCreatedEvent> context)
+        {
+            var stockResult = new List<bool>();
+
+            foreach (var item in context.Message.OrderItems)
+            {
+                stockResult.Add(await _appDbContext.Stocks.AnyAsync(x => x.ProductId == item.ProductId && x.Count > item.Count));
+            }
+
+            if (stockResult.All(x => x.Equals(true)))
+            {
+                foreach (var item in context.Message.OrderItems)
+                {
+                    var stock = await _appDbContext.Stocks.FirstOrDefaultAsync(x => x.ProductId == item.ProductId);
+                    if (stock != null)
+                    {
+                        stock.Count -= item.Count;
+
+                        await _appDbContext.SaveChangesAsync();
+
+                        _logger.LogInformation($"Stock was reserved for Buyer Id : {context.Message.BuyerId}");
+
+                        var sendEndpoint = await _sendEndpointProvider.GetSendEndpoint(new Uri($"queue:{RabbitMQSettings.StockReservedEventQueueName}"));
+
+                        var stockReservedEvent = new StockReservedEvent
+                        {
+                            Payment = context.Message.PaymentMessage,
+                            BuyerId = context.Message.BuyerId,
+                            OrderId = context.Message.OrderId,
+                            OrderItems = context.Message.OrderItems
+                        };
+
+                       await sendEndpoint.Send(stockReservedEvent);
+                    }
+                }
+            }
+            else
+            {
+                await _publishEndpoint.Publish(new StockNotReservedEvent
+                {
+                    OrderId = context.Message.OrderId,
+                    Message = "Not enough stock"
+                });
+
+                _logger.LogInformation($"Not enough stock");
+            }
+        }
+    }
+}
--- Stock.API/Program.cs
+++ Stock.API/Program.cs
@@ -1,11 +1,46 @@
+using Core;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Stock.API.Consumers;
+using Stock.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseInMemoryDatabase("StockDb");
+});
+
+builder.Services.AddMassTransit(x => {
+
+    x.AddConsumer<OrderCreatedEventConsumer>();
+    x.UsingRabbitMq((context, cfg) => {
+
+        cfg.Host(builder.Configuration.GetConnectionString("RabbitMQ"));
+
+        cfg.ReceiveEndpoint(RabbitMQSettings.StockOrderCreatedEventQueueName, e => {
+
+            e.ConfigureConsumer<OrderCreatedEventConsumer>(context);
+        });
+    });
+
+});
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Stocks.Add(new Stock.API.Models.Stock { Id = 1, ProductId = 1, Count = 100 });
+    context.Stocks.Add(new Stock.API.Models.Stock { Id = 2, ProductId = 2, Count = 100 });
+    context.SaveChanges();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -14,31 +49,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
